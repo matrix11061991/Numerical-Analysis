2a99a64af2d995f948d2f5cb45e08927fd33def5
Jaurès RATSIMBAZAFIHARIVOLA <74584503+matrix11061991@users.noreply.github.com>
2022-12-12 15:18:28 +0200

    Update trapez.py

--- Integration/Trapeze/trapez.py
+++ Integration/Trapeze/trapez.py
@@ -1,12 +1,22 @@
-import scipy.integrate as integrate
+import math
+
+def trapèze(f, a, b, n):
+  h = (b - a) / n
+  s = 0.5 * (f(a) + f(b))
+  for i in range(1, n):
+    s += f(a + i * h)
+  return s * h
+
+# Appliquons cette méthode pour calculer l'intégrale de f(x) = x^2 sur l'intervalle [0, 2]
 
 def f(x):
-    # La fonction à intégrer
-    return x**2
+  return x * x
 
-a = 0 # limite inférieure de l'intégrale
-b = 2 # limite supérieure de l'intégrale
+a = 0
+b = 2
+n = 100
 
-approximation = integrate.trapezoid(f, a, b)
+résultat = trapèze(f, a, b, n)
 
-print(approximation)
+print("L'intégrale de f(x) = x^2 sur l'intervalle [0, 2] est approximativement égale à", résultat)
+
